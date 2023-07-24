eb27168df27b190349091b9be555357a664210be
WeberSouzaWeb <107212929+WeberSouzaWeb@users.noreply.github.com>
2023-07-24 18:49:58 -0300

    Add files via upload

--- src/components/Baseboard/index.js
+++ src/components/Baseboard/index.js
@@ -1,1 +1,10 @@
+import styles from './Baseboard.module.css';
+function Baseboard() {
+    return (
+        <footer className={styles.baseboard}>
+            <h2>Desenvolver by Weber Souza</h2>
+        </footer>
+    )
+}
 
+export default Baseboard;

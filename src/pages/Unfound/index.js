14a2c5da8c0a4fbb176a7dcbbbc9bdab92c8819d
WeberSouzaWeb <107212929+WeberSouzaWeb@users.noreply.github.com>
2023-07-24 19:05:54 -0300

    Add files via upload

--- src/pages/Unfound/index.js
+++ src/pages/Unfound/index.js
@@ -1,1 +1,12 @@
+import style from './Unfound.module.css'
 
+function Unfound(){
+    return(
+        <section className={style.container}>
+            <h2>Ops!</h2>
+            <p>O conteúdo que está procurando não foi encontradod</p>
+        </section>
+    )
+}
+
+export default Unfound;

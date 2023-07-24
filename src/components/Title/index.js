5114eff6e82af65c2027a7e85810af38d679ad92
WeberSouzaWeb <107212929+WeberSouzaWeb@users.noreply.github.com>
2023-07-24 18:55:00 -0300

    Add files via upload

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,1 +1,11 @@
+import styles from './Title.module.css'
 
+function Title({ children }) {
+    return (
+        <div className={styles.title}>
+            {children}
+        </div>
+    )
+}
+
+export default Title;

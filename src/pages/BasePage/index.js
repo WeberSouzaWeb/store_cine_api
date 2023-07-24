b9c1ffa7ecc9eb19573bf6b48d357e82d21c7b7d
WeberSouzaWeb <107212929+WeberSouzaWeb@users.noreply.github.com>
2023-07-24 19:00:38 -0300

    Add files via upload

--- src/pages/BasePage/index.js
+++ src/pages/BasePage/index.js
@@ -1,1 +1,21 @@
+import Baseboard from "components/Baseboard";
+import Container from "components/Container";
+import Header from "components/Header";
+import FavoritesProvider from "context/Favorites";
+import { Outlet } from "react-router-dom";
 
+function BasePage() {
+    return (
+        <main>
+            <Header />
+            <FavoritesProvider>
+                <Container>
+                    <Outlet />
+                </Container>
+            </FavoritesProvider>
+            <Baseboard />
+        </main>
+    )
+}
+
+export default BasePage;

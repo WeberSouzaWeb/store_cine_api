7492f51c4c2868f753d65b57e458f1e6dd85478f
WeberSouzaWeb <107212929+WeberSouzaWeb@users.noreply.github.com>
2023-07-24 16:22:17 -0300

    Add files via upload

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,1 +1,9 @@
-// author: Weber Souza
+import styles from './Banner.module.css';
+
+function Banner({imagem}) {
+    return (
+        <div className={styles.cover}
+            style={{ backgroundImage: `url('imagens/banner-${imagem}.png')` }}></div>
+    )
+}
+export default Banner;

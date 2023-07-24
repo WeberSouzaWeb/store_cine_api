384822ec160f42e9eb59a01e1a143d32a1c7be2a
WeberSouzaWeb <107212929+WeberSouzaWeb@users.noreply.github.com>
2023-07-24 18:50:59 -0300

    Add files via upload

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,1 +1,25 @@
+import { useFavoriteContext } from 'context/Favorites';
+import styles from './Card.module.css'
+import iconeFavoritar from './favoritar.png'
+import iconeDesfavoritar from './desfavoritar.png'
+import { Link } from 'react-router-dom';
 
+function Card({ id, title, cover }) {
+    const { favorite, addFavorite } = useFavoriteContext();
+    const isFavorite = favorite.some((fav) => fav.id === id);
+    const icone = !isFavorite ? iconeFavoritar : iconeDesfavoritar;
+    return (
+        <div className={styles.container}>
+            <Link className={styles.link} to={`/${id}`}>
+                <img src={cover} alt={title} className={styles.cover} />
+                <h2>{title}</h2>
+            </Link>
+            <img src={icone} alt="Favoritar filme" className={styles.favoritar}
+                onClick={() => {
+                    addFavorite({ id, title, cover })
+                }} />
+        </div>
+    )
+
+}
+export default Card;

1b6440a18c6b707f3ad6304730ad3126f99f7bfa
WeberSouzaWeb <107212929+WeberSouzaWeb@users.noreply.github.com>
2023-07-24 18:53:06 -0300

    Add files via upload

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,24 @@
+import { Link } from "react-router-dom";
+import HeaderLink from "components/HeaderLink";
+import styles from './Header.module.css';
+import logo from './logo.png';
 
+function Header() {
+    return (
+        <header className={styles.header}>
+            <Link to="./">
+                <img src={logo} alt="store logo"></img>
+            </Link>
+            <nav>
+                <HeaderLink url="./">
+                    Home
+                </HeaderLink>
+                <HeaderLink url="./Favorites" >
+                    Favorites
+                </HeaderLink>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;

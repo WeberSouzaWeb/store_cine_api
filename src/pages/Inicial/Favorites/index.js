d1843444e622d78b11447cb69ae9c0ae0af6da2b
WeberSouzaWeb <107212929+WeberSouzaWeb@users.noreply.github.com>
2023-07-24 19:03:17 -0300

    Add files via upload

--- src/pages/Inicial/Favorites/index.js
+++ src/pages/Inicial/Favorites/index.js
@@ -1,1 +1,20 @@
+import Banner from 'components/Banner';
+import styles from './Favorites.module.css'
+import Card from 'components/Card';
+import { useFavoriteContext } from 'context/Favorites';
 
+function Favorites() {
+    const { favorite } = useFavoriteContext();
+    return (
+        <>
+            <Banner imagem='favoritos' />
+            <section className={styles.container}>
+                {favorite.map((fav) => {
+                    return <Card {...fav} key={fav.id} />
+                })}
+            </section>
+        </>
+    )
+}
+
+export default Favorites;
